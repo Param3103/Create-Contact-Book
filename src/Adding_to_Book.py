a8951563adc60db99e2880374c34bb74fbd44bcf
Param3103 <param.mehrotra@gmail.com>
2020-12-26 20:20:02 +0800

    commiting class adding to book

--- src/Adding_to_Book.py
+++ src/Adding_to_Book.py
@@ -12,14 +12,15 @@ class AddingToBook:
         return(contact_book)
 
 # need to work on this
-    def update_existing_contact(identifying_keys, identifying_values, tbc_keys, new_values, contact_book_writer, contact_book_reader):
+    def update_existing_contact(self, identifying_keys, identifying_values, tbc_keys, new_values, contact_book_reader):
         for key in identifying_keys:
-            for contact in contact_book_reader: #something is wrong here
-                if contact.key == identifying_values[identifying_keys.index(key)]:
-                    for tbc_key in tbc_keys:
-                        original_contact = contact
-                        contact.tbc_key = new_values[tbc_keys.index(tbc_key)]
-                        contact_book_writer[contact_book.index(original_contact)] = contact
-        return(contact_book_writer)
+            for contacts in contact_book_reader:
+                for contact in contacts: #something is wrong here
+                    if contact.key == identifying_values[identifying_keys.index(key)]:
+                        for tbc_key in tbc_keys:
+                            original_contact = contact
+                            contact.tbc_key = new_values[tbc_keys.index(tbc_key)]
+                            self[contact_book_reader.index(original_contact)] = contact
+        return(self)
 
 AddingToBook
