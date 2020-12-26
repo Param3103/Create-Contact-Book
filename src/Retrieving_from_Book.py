15b11f906c611717f26e5dfe8515233f0f5425ac
Param3103 <param.mehrotra@gmail.com>
2020-12-26 17:07:52 +0800

    commtting changes to file retrieveing from book

--- src/Retrieving_from_Book.py
+++ src/Retrieving_from_Book.py
@@ -19,19 +19,4 @@ class RetrievingFromBook:
             else:
                 continue
         return(contacts)
-
-    def update_existing_contact(self, identifying_keys, identifying_values, tbc_keys, updated_values, contact_book):
-        for contact in contact_book:
-            found = True
-            for identifying_key in identifying_keys:
-                if (contact[identifying_key] != identifying_values[identifying_keys.index(identifying_key)]):
-                    found = False
-            if found is True:
-                for tbc_key in tbc_keys:
-                    contact_index = contact_book.index(contact)
-                    contact[tbc_key] = updated_values[tbc_keys.index(tbc_key)]
-                    contact_book[contact_index] = contact
-            else:
-                continue
-        return(contact_book)
 RetrievingFromBook
