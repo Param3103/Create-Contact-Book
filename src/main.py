c6b19217c9ae528795611e351748754c3921111f
Param3103 <param.mehrotra@gmail.com>
2020-12-26 19:35:40 +0800

    still need to work on object update_existing_contact

--- src/main.py
+++ src/main.py
@@ -6,5 +6,4 @@ with open('Contact_Book.csv', 'w') as file:
     fieldnames = ['Name', 'Phone No', 'Email ID', 'Address']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     reader = csv.DictReader(file)
-    AddingToBook.creating_new_contact(AddingToBook('Param', '94276963', None, None), writer)
-   # AddingToBook.update_existing_contact(['Name'], ['Param'], ['Email ID'], ['param.mehrotra@gmail.com'], writer)
+    AddingToBook.update_existing_contact(['Name'], ['Param'], ['Email ID'], ['param.mehrotra@gmail.com'], reader)
