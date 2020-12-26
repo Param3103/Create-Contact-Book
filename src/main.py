a35161351b556c2b5060b38c639288b6f35f49cc
Param3103 <param.mehrotra@gmail.com>
2020-12-26 20:21:16 +0800

    committing changes to main code

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ with open('Contact_Book.csv', 'w') as file:
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     reader = csv.DictReader(file)
     writer = AddingToBook.creating_new_contact(AddingToBook('Param' , '94276963', None, None), writer)
-    writer = AddingToBook.update_existing_contact(['Name'], ['Param'], ['Email ID'], ['param.mehrotra@gmail.com'], writer, reader)
+    writer = AddingToBook.update_existing_contact(writer, ['Name'], ['Param'], ['Email ID'], ['param.mehrotra@gmail.com'], reader)
