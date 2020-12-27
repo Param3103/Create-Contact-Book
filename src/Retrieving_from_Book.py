064afa8f7a22338d3ff8030b2a168573f79bc6c4
Param3103 <param.mehrotra@gmail.com>
2020-12-27 19:52:53 +0800

    g

--- src/Retrieving_from_Book.py
+++ src/Retrieving_from_Book.py
@@ -2,18 +2,6 @@ import csv
 
 class RetrievingFromBook:
     def find_contact(contact_book, finder_keys, finder_values):
-        contact_book = csv.DictReader(contact_book)
-        contacts = []
         for row in contact_book:
-            contact = dict(row)
-            print(contact)
-            found = True
-            for finder_key in finder_keys:
-                if contact[finder_key] != finder_values[finder_keys.index(finder_key)]:
-                    found = False
-            if found:
-                contacts.append(contact)
-            else:
-                continue
-        return(contacts)
+            print(row)
 RetrievingFromBook 
--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ import csv
 from Adding_to_Book import AddingToBook
 from Retrieving_from_Book import RetrievingFromBook
 # creating contact_book
-with open('Contact_Book.csv', 'w') as file:
+with open('Contact_Book.csv', 'r+') as file:
     fieldnames = ['Name', 'Phone No', 'Email ID', 'Address']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    reader = csv.DictReader(file)
-    writer = AddingToBook.creating_new_contact('Param', '94276963', None, None, writer)
+    writer = csv.writer(file)
+    reader = csv.reader(file)
+with open('Contact_Book.csv', 'a+') as file:
+    writer = csv.writer(file)
+    AddingToBook.creating_new_contact('Param', '94276963', None, None, writer)
