c0979a9709fb17fa330f0e7eb79bdf925da1d635
Param3103 <param.mehrotra@gmail.com>
2020-12-27 20:33:41 +0800

    commiting changes to main

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ with open('Contact_Book.csv', 'r+') as file:
     fieldnames = ['Name', 'Phone No', 'Email ID', 'Address']
     writer = csv.writer(file)
     reader = csv.reader(file)
+    AddingToBook.creating_new_contact('Param', '+65 94276963', None, None, writer)
 with open('Contact_Book.csv', 'a+') as file:
     writer = csv.writer(file)
-    AddingToBook.creating_new_contact('Param', '94276963', None, None, writer)
+    reader = csv.reader(file)
+    print(RetrievingFromBook.find_contact(reader, ['Name'], ['Param']))
