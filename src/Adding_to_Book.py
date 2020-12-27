9dd3b46c24aaeebe9b94e9dbb18ffa504c6d99e7
Param3103 <param.mehrotra@gmail.com>
2020-12-27 19:50:26 +0800

    commiting adding to book file

--- src/Adding_to_Book.py
+++ src/Adding_to_Book.py
@@ -2,13 +2,12 @@ import csv
 class AddingToBook:
 
     def creating_new_contact(name, phone, email, address, contact_book):
-        contact = {'Name' : name, 'Phone No' : phone, 'Address' : address, 'Email ID' : email}
+        contact = [name, phone, address, email]
         contact_book.writerow(contact)
-        return(contact_book)
+        return contact_book
 
 # need to work on this
-    def update_existing_contact(self, identifying_keys, identifying_values, tbc_keys, new_values, contact_book_reader):
+    def update_existing_contact(self, identifying_keys, identifying_values, tbc_keys, new_values, contact_book_reader, contact_book_writer):
         pass
-
 
 AddingToBook
