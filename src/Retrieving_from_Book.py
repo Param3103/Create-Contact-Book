a19c5a6f4ca7a1ee295838c4c7c6cc77029f213b
Param3103 <param.mehrotra@gmail.com>
2020-12-27 20:33:24 +0800

    commitingg changes to retrievinnf file

--- src/Retrieving_from_Book.py
+++ src/Retrieving_from_Book.py
@@ -1,7 +1,21 @@
 import csv
 
+
 class RetrievingFromBook:
     def find_contact(contact_book, finder_keys, finder_values):
+        contacts = []
         for row in contact_book:
-            print(row)
-RetrievingFromBook 
+            contact = dict(row)
+            print(contact)
+            found = True
+            for finder_key in finder_keys:
+                if contact[finder_key] != finder_values[finder_keys.index(finder_key)]:
+                    found = False
+            if found:
+                contacts.append(contact)
+            else:
+                continue
+        return (contacts)
+
+
+RetrievingFromBook
