583e3e1c6cab39530590f1c44d0a3ac292e8b7f1
Param3103 <73064464+Param3103@users.noreply.github.com>
2020-12-27 16:47:24 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,4 +6,4 @@ with open('Contact_Book.csv', 'w') as file:
     fieldnames = ['Name', 'Phone No', 'Email ID', 'Address']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     reader = csv.DictReader(file)
-    writer = AddingToBook.creating_new_contact('Param', '94276963', None, None, writer) 
+    writer = AddingToBook.creating_new_contact('Param', '94276963', None, None, writer)  
