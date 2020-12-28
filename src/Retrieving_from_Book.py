aef234992ae0491c9db4a239502bbf3352597065
Param3103 <param.mehrotra@gmail.com>
2020-12-28 12:04:28 +0800

    committing changes to code that retrieves contact

--- src/Retrieving_from_Book.py
+++ src/Retrieving_from_Book.py
@@ -3,18 +3,24 @@ import csv
 
 class RetrievingFromBook:
     def find_contact(contact_book, finder_keys, finder_values):
+        fieldnames = ['Name', 'Phone No', 'Email ID', 'Address']
         contacts = []
-        for row in contact_book:
-            contact = dict(row)
-            print(contact)
-            found = True
-            for finder_key in finder_keys:
-                if contact[finder_key] != finder_values[finder_keys.index(finder_key)]:
-                    found = False
-            if found:
-                contacts.append(contact)
+        for contact in contact_book:
+            if len(contact) == 0:
+                pass
             else:
-                continue
+                temp = contact
+                found = True
+                for finder_key in finder_keys:
+                    if contact[fieldnames.index(finder_key)] != finder_values[finder_keys.index(finder_key)]:
+                        found = False
+                        contact = temp
+                    else:
+                        contact = temp
+                if found:
+                    contacts.append(contact)
+                else:
+                    continue
         return (contacts)
 
 
