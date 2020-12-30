af91047aba001845dcf7daec3abc41e458ac6e91
Param3103 <param.mehrotra@gmail.com>
2020-12-30 17:50:05 +0800

    updating changes in main code

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import csv
 from Adding_to_Book import AddingToBook
 from Retrieving_from_Book import RetrievingFromBook
-
 # creating contact_book
 
-#this code adds data
+
 """
+#this code adds data
 with open('Contact_Book.csv', 'a') as file:
     fieldnames = ['Name', 'Phone No', 'Email ID', 'Address']
     writer = csv.writer(file)
