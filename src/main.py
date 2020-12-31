639fedd715b5c8eb07882b34447bb412c54854bd
Param3103 <param.mehrotra@gmail.com>
2020-12-31 13:05:01 +0800

    commiting updates to project

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import csv
 from Adding_to_Book import AddingToBook
 from Retrieving_from_Book import RetrievingFromBook
 import os
+from Contact import Contact
 # creating contact_book
 
 
@@ -10,9 +11,9 @@ import os
 """
 with open('Contact_Book.csv', 'a') as file:
     writer = csv.writer(file)
-    AddingToBook.creating_new_contact('Charu', '+65 96826564', None, None, writer)
-
+    AddingToBook.create_new_contact('Param', '+65 96826564', None, None, writer)
 """
+
 """
 #this code retrieves data
 
@@ -22,15 +23,16 @@ with open('Contact_Book.csv', 'r+') as file:
         print(contact.name)
 
 """
-"""
+
 #this code updates data into same file
 
 with open('Contact_book.csv', 'r+') as reader_file:
     with open('Updated_Contact_Book.csv', 'w+') as writer_file:
         writer = csv.writer(writer_file)
         reader = csv.reader(reader_file)
+        AddingToBook.create_new_contact(Contact('Param', None, None, None), writer)
         AddingToBook.update_existing_contact(['Name'], ['Param'], ['Email ID'], ['param.mehrotra@gmail.com'], reader, writer)
 os.remove('Contact_Book.csv')
 os.rename('Updated_Contact_book.csv', 'Contact_Book.csv')
-"""
+
 
