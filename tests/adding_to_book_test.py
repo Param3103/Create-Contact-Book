9c3bf5153b519e5a15cb453e42b582674be3589e
Param3103 <param.mehrotra@gmail.com>
2021-01-03 16:56:54 +0800

    commiting first test

--- tests/adding_to_book_test.py
+++ tests/adding_to_book_test.py
@@ -12,7 +12,7 @@ class AddingToBookTest(unittest.TestCase):
     def test_create_new_contact(self):
         # first create contact object from input
         contact = Contact('Rajesh', '+65 12345678', None, None)
-        with open('src.Contact_Book.csv', 'r+') as file:
+        with open('Contact_Book.csv', 'r+') as file:
             csv_writer = csv.writer(file)
             AddingToBook.create_new_contact(contact, csv_writer)
         if contact.name == None:
@@ -25,8 +25,11 @@ class AddingToBookTest(unittest.TestCase):
             contact.address = ''
 
         #here check if csv file has contact or not
-        for line in open('src\Contact_Book.csv', 'r'):
-            self.assertEqual((str(contact.name)+','+str(contact.phone)+','+str(contact.email)+','+str(contact.address)), line)
+        with open('Contact_Book.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if len(row) > 0:
+                    self.assertEqual(row, [str(contact.name),str(contact.phone),str(contact.email),str(contact.address)])
 
     def test_updating_contact(self):
         pass
