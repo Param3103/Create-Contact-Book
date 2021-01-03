d679db31a7d9a3a7ea8238e71b3661308d7016e1
Param3103 <param.mehrotra@gmail.com>
2021-01-03 19:20:24 +0800

    commiting new tests

--- tests/retrieving_from_book_test.py
+++ tests/retrieving_from_book_test.py
@@ -1,8 +1,20 @@
 import unittest
+from src.Adding_to_Book import AddingToBook
+from src.Retrieving_from_Book import RetrievingFromBook
+from src.Contact import Contact
+import csv
+import os
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class RetrievingFromBookTest(unittest.TestCase):
+    def SetUp(self):
+        contact = Contact('Param', None, None, None)
+        AddingToBook.create_new_contact(contact)
+
+    def test_find_contact(self):
+        contacts = RetrievingFromBook.find_contact(['Name'], ['Param'])
+        for contact in contacts:
+            self.assertEqual(contact, ['Param', None, None, None])
 
 if __name__ == '__main__':
     unittest.main()
